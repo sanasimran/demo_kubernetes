8d59efd5328e023763e4f92b65428e12b7cb193b
sanasimran <49567407+sanasimran@users.noreply.github.com>
2021-05-08 21:19:08 +0530

    add backend to the branch

---
+++ backend/nodebackend/index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+
+mongoose.connect('mongodb://localhost/node_auth', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, () => {
+    console.log('connected to the database')
+})
+
+const routes = require('./routes/routes')
+
+app = express()
+
+app.use(cookieParser())
+app.use(cors({
+    credentials: true,
+    origin: ['http://localhost:3000']
+
+}))
+
+
+app.use(express.json())
+
+app.use('/api', routes)
+
+app.listen(8000)
