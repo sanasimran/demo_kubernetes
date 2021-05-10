9e1170f9830cf1f05c44b04220e7611657164677
sanasimran <49567407+sanasimran@users.noreply.github.com>
2021-05-10 22:41:54 +0530

    add search function

---
+++ search/src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import './App.css';
+import Recipes from "./components/Recipes";
+
+
+class App extends Component {
+  state = {
+    recipes: [],
+    isSubmitted:  false
+  }
+  getRecipe = async (e) => {
+    const recipeName = e.target.elements.recipeName.value;
+    e.preventDefault();
+    const api_call = await fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${recipeName}`);
+    const data = await api_call.json();
+    this.setState({ recipes: data });
+    console.log(api_call);
+    this.setState({isSubmitted: true});
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title"> FOODIE Recipe Search</h1>
+          <button className="button">Log Out</button>
+        </header>
+        <form onSubmit={this.getRecipe.bind(this)} style={{ marginBottom:"2rem" }}>
+          <input className="form__input" type="text" name="recipeName" />
+          <button className="form__button">Search</button>
+        </form>
+        {this.state.isSubmitted && <Recipes recipes={this.state.recipes.meals} />}
+      </div>
+    );
+  }
+}
+
+export default App;
