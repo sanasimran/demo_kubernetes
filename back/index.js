49bec2b7310ad13066d2c9fcf00dd190bad38e9e
sanasimran <49567407+sanasimran@users.noreply.github.com>
2021-05-10 22:38:15 +0530

    add backend

---
+++ back/index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+dotenv.config();
+app.use(cors());
+
+// connect to db
+mongoose.connect(
+  process.env.DB_CONNECT,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+  () => console.log("connected to db")
+);
+
+// import routes
+const authRoutes = require("./routes/auth");
+const dashboardRoutes = require("./routes/dashboard");
+const verifyToken = require("./routes/validate-token");
+
+// middlewares
+app.use(express.json()); // for body parser
+
+// route middlewares
+app.use("/api/user", authRoutes);
+app.use("/api/dashboard", verifyToken, dashboardRoutes);
+
+
+
+app.listen(3001, () => console.log("server is running..."));
