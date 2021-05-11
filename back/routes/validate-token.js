4a86a8c4bc39cdfdde4963ad3e8a2dfbda6826f3
Nawaz <k.nawaz.h@gmail.com>
2021-05-11 13:20:26 -0700

    add logging,include env,require dotenv

--- back/routes/validate-token.js
+++ back/routes/validate-token.js
@@ -2,14 +2,17 @@ const jwt = require("jsonwebtoken");
 
 // middleware to validate token
 const verifyToken = (req, res, next) => {
+  console.log("Validating token :: " + req.header("auth-token"))
   const token = req.header("auth-token");
   if (!token) return res.status(401).json({ error: "Access denied" });
 
   try {
     const verified = jwt.verify(token, process.env.TOKEN_SECRET);
     req.user = verified;
+    console.log("Token verified successfully");
     next();
   } catch (err) {
+    console.log("Token verification failed");
     res.status(400).json({ error: "Token is not valid" });
   }
 };
