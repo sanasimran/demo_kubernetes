839499d13bbcd338dc5137907ee40166efed2af1
Nawaz <k.nawaz.h@gmail.com>
2021-05-11 20:08:57 -0700

    remove commented out code

--- front/src/config.js
+++ front/src/config.js
@@ -5,9 +5,4 @@ const config = {
   searchUrl: process.env.REACT_APP_SEARCH_URL,
 };
 
-// const config = {
-//   baseUrl: "http://backend",
-//   searchUrl: "http://search:5000",
-// };
-
 export default config;
